95dd794458fe66eff4d08126c2e49c4741e31cbe
Harshit Soni <harshitsoni333@gmail.com>
2020-02-23 16:59:24 +0530

    json error try 4

---
+++ src/main/java/com/soni/usermanagement/ErrorMessage.java
@@ -0,0 +1,44 @@
+package com.soni.usermanagement;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ErrorMessage {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss")
+    private LocalDateTime timestamp;
+    private String message;
+    private int error;
+    
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getError() {
+        return error;
+    }
+
+    public void setError(int error) {
+        this.error = error;
+    }
+
+    public ErrorMessage(LocalDateTime timestamp, String message, int error) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.error = error;
+    }
+}
---
+++ src/main/java/com/soni/usermanagement/ExceptionsHandler.java
@@ -0,0 +1,25 @@
+package com.soni.usermanagement;
+
+import java.time.LocalDateTime;
+import com.soni.usermanagement.exception.classes.UserNotFoundException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionsHandler extends ResponseEntityExceptionHandler{
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest request) {
+
+        ErrorMessage errorMessage = new ErrorMessage(LocalDateTime.now(), ex.getMessage(), 404);
+        
+        return new ResponseEntity<>(
+            errorMessage, new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+}
