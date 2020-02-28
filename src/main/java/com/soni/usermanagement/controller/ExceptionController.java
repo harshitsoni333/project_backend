7edb6c45f442e420c8cd32253ecf1636d74bcbdb
Harshit Soni <harshitsoni333@gmail.com>
2020-02-28 17:56:50 +0530

    put file exceptions successful

--- src/main/java/com/soni/usermanagement/controller/ExceptionController.java
+++ src/main/java/com/soni/usermanagement/controller/ExceptionController.java
@@ -8,6 +8,7 @@ import com.soni.usermanagement.exception.error.FileAlreadyExists;
 import com.soni.usermanagement.exception.error.NoFilesFound;
 import com.soni.usermanagement.exception.error.NoUsersFoundException;
 import com.soni.usermanagement.exception.error.UserNotFoundException;
+import com.soni.usermanagement.exception.success.FileUpdated;
 import com.soni.usermanagement.exception.success.NewFileAdded;
 import com.soni.usermanagement.exception.success.NewUserAdded;
 import com.soni.usermanagement.exception.success.UserDeleted;
@@ -55,7 +56,7 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.CONFLICT);
     }
 
-    @ExceptionHandler({NewUserAdded.class, UserUpdated.class, UserDeleted.class, NewFileAdded.class})
+    @ExceptionHandler({NewUserAdded.class, UserUpdated.class, UserDeleted.class, NewFileAdded.class, FileUpdated.class})
     public ResponseEntity<Object> newUserAddedHandler(Exception ex, WebRequest request) {
 
         ErrorMessage errorMessage = new ErrorMessage(
