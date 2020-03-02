b417ce72fc2abe234f5d7a7f26e75bdbe737136f
Harshit Soni <harshitsoni333@gmail.com>
2020-03-02 21:22:02 +0530

    made emailvalidation pojo

--- src/main/java/com/soni/usermanagement/controller/AppManagementController.java
+++ src/main/java/com/soni/usermanagement/controller/AppManagementController.java
@@ -14,6 +14,7 @@ import com.soni.usermanagement.exception.success.AppDeleted;
 import com.soni.usermanagement.exception.success.AppUpdated;
 import com.soni.usermanagement.exception.success.NewAppAdded;
 import com.soni.usermanagement.model.AppManagement;
+import com.soni.usermanagement.model.EmailValidation;
 import com.soni.usermanagement.repository.AppManagementRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,25 +30,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class AppManagementController {
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // E-Mail Validation function
-    private static final String EMAIL_REGEX = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
-
-    public static boolean emailValidator(String email) {
-
-        if (email == null) {
-            return false;
-        }
-
-        Matcher matcher = EMAIL_PATTERN.matcher(email);
-        return matcher.matches();
-    }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     @Autowired
     private AppManagementRepo repo;
@@ -72,7 +54,7 @@ public class AppManagementController {
         List<String> contacts = Arrays.asList(newApp.getContacts().split(";[ ]*"));
         if(!newApp.getContacts().equals(""))
         for(String contact: contacts) {
-            if(!emailValidator(contact)) {
+            if(!EmailValidation.emailValidator(contact)) {
                 // e-mail is not valid
                 throw new EmailNotValidException(contact);
             }
@@ -111,7 +93,7 @@ public class AppManagementController {
         if(!newApp.getContacts().equals(""))
         for(String email: emails) {
             // if email not valid
-            if (!emailValidator(email)) {
+            if (!EmailValidation.emailValidator(email)) {
                 throw new EmailNotValidException(email);
             }
         }
