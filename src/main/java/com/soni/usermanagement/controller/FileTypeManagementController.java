18499ed67604cdfc40746419894fec1858931f43
Harshit Soni <harshitsoni333@gmail.com>
2020-03-05 23:16:04 +0530

    made half of filetypemanagement

--- src/main/java/com/soni/usermanagement/controller/FileTypeManagementController.java
+++ src/main/java/com/soni/usermanagement/controller/FileTypeManagementController.java
@@ -15,11 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,10 +43,14 @@ public class FileTypeManagementController {
     @PostMapping("/fileTypes")
     public ResponseEntity<String> addFileType(@Valid @RequestBody FileTypeManagement newFileType) {
 
-        // checking for alphanumeric value of fileTypeCode
+        // checking constraints of fileTypeCode
+        // should be alphanumeric
         String fileTypeCode = newFileType.getFileTypeCode();
         if(!IsAlphaNumeric.isAlphaNumeric(fileTypeCode))
         throw new InvalidEntry(fileTypeCode);
+        // should have length <= 6
+        if(fileTypeCode.length() > 6) 
+        throw new InvalidEntry(String.format("length of '%s' is more than 6 characters", fileTypeCode));
 
         // checking for invalid values of isBankFile and isKMT54
         String isBankFile = newFileType.getIsBankFile();
--- src/main/java/com/soni/usermanagement/exception/error/InvalidEntry.java
+++ src/main/java/com/soni/usermanagement/exception/error/InvalidEntry.java
@@ -4,6 +4,6 @@ public class InvalidEntry extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     public InvalidEntry(String entry) {
-        super("Enter is invalid: " + entry);
+        super("Entry is invalid: " + entry);
     }
 }
